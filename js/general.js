b798d5322216ec41ee46fe617c4713478b6ce79d
pernould <pernould@gmail.com>
2017-01-19 13:59:00 +0100

    test
    test

--- js/general.js
+++ js/general.js
@@ -11,16 +11,12 @@ $(function() {
 			var PositionMenu 		= $('#menu').offset().top;
 			var scrollPosition		= $(window).scrollTop();
 			$('nav').removeClass("fixed-bottom");
-			console.log("window scrollTOP: "+$(window).scrollTop());
-			console.log("PositionMenu: "+PositionMenu);
 			if(scrollPosition > PositionMenu) {
 				// position fixe en haut
-				console.log("> 0 !");
 				$('nav').addClass("fixed-top");
 			} else {
 				// position normale
 				$('nav').removeClass("fixed-top");
-				console.log("remove fixed-top");
 			}
 			//    
 		}
